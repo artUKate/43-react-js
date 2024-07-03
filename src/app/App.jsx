19e4647ab17f1e692c598fd3a9f02cd5e3fb7da0
artUKate <shkolnidz@gmail.com>
2024-07-03 19:46:29 +0200

    ádded a  profile

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,7 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className='App-header'>
       <h1>Hello, react 🚀</h1>
+      </header>
+      <main className='App-main'>
+<div className='profile-card'>
+<img src="photo_2024-07-03_18-15-51.jpg" alt="Profile" className="profile-image" />
+<h2>Kateryna Ushenko</h2>
+<p>Hi! I'm studying to be a developer and I hope that this will be my profession soon. Here are a few of my hobbies:</p>
+          <ul>
+            <li>Reading</li>
+            <li>Drawing</li>
+            <li>Travels</li>
+          </ul>
+</div>
+      </main>
+      <footer className="App-footer">
+        <p>  All rights reserved.</p>
+      </footer>
     </div>
   );
 }
